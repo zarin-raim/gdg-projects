e51d32b133b7b6d20551c8053e6e1059cde214f0
zarin-raim <zarinraim@gmail.com>
2021-09-18 13:09:10 +0200

    Android Basics in Kotlin > Kotlin Basics > Add a Button > The dice roll logic

--- DiceRoller/app/src/main/java/raim/zarin/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/raim/zarin/diceroller/MainActivity.kt
@@ -1,11 +1,56 @@
 package raim.zarin.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+
+        rollButton.setOnClickListener {
+            /* // HOW TO SHOW A TOAST
+               // 1. Long version
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+               // 2. Shortened version*/
+            Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+
+            rollDice()
+        }
+    }
+
+    /**
+     * Roll the dice and update the screen with the result
+     */
+    private fun rollDice() {
+        // Create a new 6-sided dice and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+/**
+ * Dice logic class
+ */
+class Dice(private val numSides: Int = 6) {
+    /**
+     * Rolls the dice and returns a result
+     */
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
